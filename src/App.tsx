fa4be6ee5661bb3733f2c9ed37bc00b40154dd5a
SergioRubio01 <100428611@alumnos.uc3m.es>
2024-10-07 16:54:00 +0200

    Added React component to: - Documentation - Pricing - About

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,9 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './pages/LandingPage';
 import LoginPage from './pages/LoginPage';
 import Dashboard from './pages/Dashboard';
-// import About from './pages/About';
-// import Pricing from './pages/Pricing';
-// import {Documentation,Contributors} from './pages/Documentation';
+import About from './pages/About';
+import Pricing from './pages/Pricing';
+import Documentation from './pages/Documentation';
 
 function App() {
   return (
@@ -13,10 +13,10 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        {/* <Route path="/about" element={<About />} /> */}
-        {/* <Route path="/pricing" element={<Pricing />} /> */}
-        {/* <Route path="/docs" element={<Documentation />} /> */}
-        {/* <Route path="/docs/contributors" element={<Contributors />} /> */}
+        <Route path="/about" element={<About />} />
+        <Route path="/pricing" element={<Pricing />} />
+        <Route path="/docs" element={<Documentation />} />
+        {/* <Route path="/docs/contributors" element={<Contributors />} */}
       </Routes>
     </Router>
   );
